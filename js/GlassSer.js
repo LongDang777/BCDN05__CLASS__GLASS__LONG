92455bedc85f088d6b8e0104179bfd9ba6da0706
Long <dangtienlong2020@gmail.com>
2022-02-14 14:11:28 +0700

    clean code

--- js/GlassSer.js
+++ js/GlassSer.js
@@ -2,8 +2,6 @@ export default class GlassesSer {
     constructor(){
     }
     layDSGlass(){
-      // let promise = axios();
-      // return promise;
       return axios({
           method: 'get',
           url: 'https://620274b3b8735d00174cbad6.mockapi.io/Glass'
--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 import GlassesSer from "./GlassSer.js";
 import Glass from "./Glass.js";
 
-// let glassShow = new Glass();
 let glassSer = new GlassesSer();
-let glass = new Glass();
 let vglassesList = document.getElementById("vglassesList");
 let hienThiGlass = (mang) => {
   for (let glass of mang) {
-    let { id, src, virtualImg, brand, ten, color, price, description } = glass;
+    let { id, src } = glass;
     vglassesList.innerHTML += `
     <button onclick="getGlass('${id}')" class="btn btn-white col-4">              
         <img class="img-fluid" src="./img/${src}">             
@@ -32,20 +30,16 @@ let getGlass = (id) => {
     .xemGlass(id)
     .then((result) => {
       document.querySelector(".vglasses__info").style.display = "block";
-      let { id, src, virtualImg, brand, name, color, price, description } =
+      let {virtualImg, brand, name, color, price, description } =
         result.data;
       document.getElementById(
         "avatar"
       ).innerHTML = `<img class="img-fluid" src="./img/${virtualImg}">`;
-
       document.getElementById("glassesInfo").innerHTML = `
       <h4> ${name} - ${brand} (${color})</h4> 
-
         <button class="btn btn-danger mt-2 mr-3">$${price}</button 
-        <p class=" alert-info text text-success">Stocking</p>
-        <p>${description}</p>
-      
-    `;
+        <p>Stocking</p>
+        <p>${description}</p>`;
     })
     .catch((err) => {
       console.log(err);
